017f026c72d6d9f67297d40a5ef9c7fa35bf0d66
ShahabAthar25 <shahabathar25@gmail.com>
2021-09-18 23:16:18 +0500

    Add face recognition

--- face_recognition_pic.py
+++ face_recognition_pic.py
@@ -5,10 +5,10 @@ from face_recognition.api import face_locations
 
 KNOWN_FACES_DIR = "known_faces"
 UNKNOWN_FACES_DIR = "unknown_faces"
-TOLERANCE = 0.6
+TOLERANCE = 0.4
 FRAME_THICKNESS = 3
 FONT_THICKNESS = 2
-MODEL = "cnn" # hog
+MODEL = "hog" # cnn
 
 print("Loading Known Faces")
 
@@ -37,15 +37,25 @@ for filename in os.listdir(UNKNOWN_FACES_DIR):
             match = known_names[results.index(True)]
             print(f"match found: {match}")
 
-            # top_left = (face_location[3], face_location[0])
-            # bottom_right = (face_location[1], face_location[2])
+            top_left = (face_location[3], face_location[0])
+            bottom_right = (face_location[1], face_location[2])
 
-            # color = [0, 255, 0]
+            color = [0, 255, 0]
 
-            # cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+            cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
 
-            # top_left = (face_location[3], face_location[2])
-            # bottom_right = (face_location[1], face_location[2]+22)
+            top_left = (face_location[3], face_location[2])
+            bottom_right = (face_location[1], face_location[2]+22)
 
-            # cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
-            # cv2.putText(image)
+            cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+            cv2.putText(
+                image,
+                match,
+                (face_location[3]+10, face_location[2]+15),
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.5,
+                (200, 200, 200),
+                FONT_THICKNESS)
+    cv2.imshow(filename, image)
+    cv2.waitKey(0)
+    cv2.destroyWindow(filename)
