eff1d85e68417f3ecfe4cc13cb743147ddd1939d
ShahabAthar25 <shahabathar25@gmail.com>
2021-09-18 16:51:39 +0500

    Add Face Recognition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import face_recognition
 import os
 import cv2
+from face_recognition.api import face_locations
 
 KNOWN_FACES_DIR = "known_faces"
 UNKNOWN_FACES_DIR = "unknown_faces"
@@ -20,3 +21,31 @@ for name in os.listdir(KNOWN_FACES_DIR):
         encoding = face_recognition.face_encodings(image) [0]
         known_faces.append(encoding)
         known_names.append(name)
+
+print("processing unknown faces")
+for filename in os.listdir(UNKNOWN_FACES_DIR):
+    image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+    locations = face_recognition.face_locations(image, model=MODEL)
+    encodings = face_recognition.face_encodings(image, locations)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    for face_encoding, face_location in zip(encodings, locations):
+        results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+        match = None
+
+        if True in results:
+            match = known_names[results.index(True)]
+            print(f"match found: {match}")
+
+            # top_left = (face_location[3], face_location[0])
+            # bottom_right = (face_location[1], face_location[2])
+
+            # color = [0, 255, 0]
+
+            # cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+
+            # top_left = (face_location[3], face_location[2])
+            # bottom_right = (face_location[1], face_location[2]+22)
+
+            # cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+            # cv2.putText(image)
