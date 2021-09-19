91143f6ed6804d0e95d81da7a11e76f08ea016a2
ShahabAthar25 <shahabathar25@gmail.com>
2021-09-19 14:21:53 +0500

    Add Web Cam

--- video_face_rec.py
+++ video_face_rec.py
@@ -2,12 +2,13 @@ import face_recognition
 import os
 import cv2
 
+video = cv2.VideoCapture(0)
+
 KNOWN_FACES_DIR = "known_faces"
-UNKNOWN_FACES_DIR = "unknown_faces"
 TOLERANCE = 0.4
 FRAME_THICKNESS = 1
 FONT_THICKNESS = 2
-MODEL = "hog" # cnn
+MODEL = "hog" # hog
 
 print("Loading Known Faces")
 
@@ -21,20 +22,19 @@ for name in os.listdir(KNOWN_FACES_DIR):
         known_faces.append(encoding)
         known_names.append(name)
 
-print("processing unknown faces")
-for filename in os.listdir(UNKNOWN_FACES_DIR):
-    image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+while True:
+    ret, image = video.read()
     locations = face_recognition.face_locations(image, model=MODEL)
     encodings = face_recognition.face_encodings(image, locations)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     for face_encoding, face_location in zip(encodings, locations):
+        top, right, bottom, left = face_location
         results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
         match = None
 
         if True in results:
             match = known_names[results.index(True)]
-            print(f"match found: {match}")
+            # print(f"match found: {match}")
 
             top_left = (face_location[3], face_location[0])
             bottom_right = (face_location[1], face_location[2])
@@ -55,6 +55,7 @@ for filename in os.listdir(UNKNOWN_FACES_DIR):
                 0.5,
                 (0, 0, 0),
                 FONT_THICKNESS)
+    
     cv2.imshow(filename, image)
-    cv2.waitKey(0)
-    cv2.destroyWindow(filename)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
